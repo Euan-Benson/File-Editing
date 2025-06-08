f1d58c713c13e264337b143a00d49a1dccd8b017
Euan-Benson <euanbson@gmail.com>
2025-06-08 16:54:48 +0100

    Add file read Add example.txt

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,43 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
         try {
+            // file should be in if statement if the first condition is to be met
             File file = new File("example.txt");
-            FileWriter Writer = new FileWriter("example.txt");
+            FileWriter writer = new FileWriter(file, true);
 
             if (file.createNewFile()) {
                 System.out.println("File successfully created.");
 
-
-
                 // Writing File
-                Writer.write("Files in Java are seriously good!!");
-                Writer.close();
+                writer.write("Files in Java are seriously good!!" + "\n");
+                writer.close();
 
                 System.out.println("Successfully written.");
             } else {
                 System.out.println("File already exists.");
 
-                Writer.write("This file has already been written to");
-                Writer.close();
+                writer.append("This file has been written to" + "\n");
+                writer.close();
+
+                // alternate method to write to file using BufferWriter
+//                BufferedWriter bw = new BufferedWriter(writer);
+//                bw.write("This file has already been written to");
+//                bw.newLine();
+//                bw.close();
             }
+
+            Scanner fileScanner = new Scanner(file);
+            while (fileScanner.hasNextLine()) {
+                System.out.println(fileScanner.nextLine());
+            }
+            fileScanner.close();
+
         } catch (IOException e) {
             System.err.println("An error has occurred.");
         }
